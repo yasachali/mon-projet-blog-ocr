7a2bc752cc5a1772b6cc535c23c3345418f514ca
VILO <charles.vilo@orange.com>
2019-09-13 16:42:44 +0200

    MAJ

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
 
+
+
 @Component({
+  /*balise pour afficher le compoment*/
   selector: 'app-root',
+  /*url  du template pour le component*/
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'mon-projet-angular';
+
+
+  post = [
+    {
+      title: 'Mon premier post',
+      content: 'Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression',
+      loveIts: 5,
+      create_at: '10/24/2017'
+    },
+    {
+      title: 'Mon deuxième post',
+      content: 'Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression',
+      loveIts: 15,
+      create_at: '10/24/2017'
+    },
+    {
+      title: 'Encore un post',
+      content: 'Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression',
+      loveIts: -2,
+      create_at: '10/24/2017'
+    },
+  ];
+  constructor() {
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { PostlistComponent } from './postlist/postlist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostlistComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
